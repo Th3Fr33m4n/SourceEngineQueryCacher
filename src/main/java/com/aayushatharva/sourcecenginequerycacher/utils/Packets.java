5271a4c338db7188ac3049689435bc073e24fede
Biceeeeepss <90601537+Biceeeeepss@users.noreply.github.com>
2022-03-10 11:42:49 +0200

    Adding support for HLDS

--- src/main/java/com/aayushatharva/sourcecenginequerycacher/utils/Packets.java
+++ src/main/java/com/aayushatharva/sourcecenginequerycacher/utils/Packets.java
@@ -20,6 +20,12 @@ public final class Packets {
                     32, 81, 117, 101, 114, 121, 0});
 
     /**
+     * FFFFFFFF6d
+     */
+    public static final ByteBuf A2S_INFO_RESPONSE_HEADER = Main.BYTE_BUF_ALLOCATOR.directBuffer()
+            .writeBytes(new byte[]{-1, -1, -1, -1, 109});
+    
+    /**
      * FFFFFFFF49
      */
     public static final ByteBuf A2S_INFO_RESPONSE_HEADER = Main.BYTE_BUF_ALLOCATOR.directBuffer()
